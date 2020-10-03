746c1cb6dda6a72c12a419d745c23399ce9a7153
OVI <OVI@DESKTOP-1I3LNP3>
2020-10-03 20:29:59 +0300

    The first commit Uploading all files

---
+++ Recapitulare_lab1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"MainClass","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"MainClass","l":"MainClass()","u":"%3Cinit%3E()"}];updateSearchResults();
